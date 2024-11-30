ccbd8a85f6c8fd3e5feae5d0f254b39563b5885d
hadigun007 <jtgunawan007@gmail.com>
2024-12-01 00:24:55 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+pertemuan 1
+
+1. Visual Studio Code
+2. Nodejs (yang mengeksekusi kode javascript)
+
+3. buat project vue js
+---
+pertemuan 2
+
+1. buka vscode
+2. install vue js (global)
+npm install -g @vue/cli
+
+3. buat project vue js baru
+vue create demolagi
+
+4. jalankan project
+cd demolagi
+npm run serve
+
+buka http://10.100.0.248:8080/
+
+
+---
+pertemuan 3
+1. buat halaman baru 
+
---
+++ demo/src/components/HalamanSaya.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>Ini halaman saya</h1>
+    <p>Angka: {{ number }}</p>
+    <button @click="increment">Tambah 1</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      number: 0,
+    };
+  },
+  methods: {
+    increment() {
+      this.number++;
+    },
+  },
+};
+</script>
+
+<style>
+h1 {
+    font-weight: bold;
+    color: red;
+}
+</style>
